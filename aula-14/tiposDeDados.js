cdc86f78ec6ba110e172644fc38f8268ca6d66cf
Daniel Cavalcante de Souza <danielcdesouza@gmail.com>
2022-05-18 20:58:21 -0300

    Up lesson 14

---
+++ aula-14/tiposDeDados.js
@@ -0,0 +1,40 @@
+// String
+let nome = 'Xuxinha'; 
+let nomeCompleto = "Xuxinha de Jesus";
+let comoGostoDeSerChamado = `Xuxinha`;
+console.log(nome);
+console.log(nomeCompleto);
+console.log(comoGostoDeSerChamado);
+
+// Number
+let idade = 29;
+let novaIdade = idade;
+console.log(idade)
+console.log(novaIdade)
+
+// Float
+let pi = 3.1415;
+let gasolina = 8.00;
+console.log(pi);
+let resultado = 10 + 10;
+console.log(resultado);
+let novoResultado = "10" + "10";
+console.log(novoResultado);
+
+// Boolean
+const verdadeiro = true; //1
+const falso = false; //0
+
+// Array
+const frutas = ["Laranja", "Maçã", "Uva"]
+console.log(frutas)
+
+let variasCoisas = [
+    true, 
+    false, 
+    0, 
+    3.1415, 
+    "Xuxinha", 
+    "Nena"
+]
+console.log(variasCoisas)
---
+++ aula-14/variaveis.js
@@ -0,0 +1,12 @@
+var nome = 29; //Não recomendado
+let idade = 29;
+console.log(idade);
+idade = 30;
+console.log(idade);
+
+const altura = 1.75;
+console.log(altura);
+// altura = 1.74; Essa atribuição 
+console.log(altura);
+
+// Criar três variáveis para: peso, altura e nome do pãe ou da mãe
