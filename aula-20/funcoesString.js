b4b331aafa65c7a670408b0195c0e2e3ae4cd7e5
Daniel Cavalcante de Souza <danielcdesouza@gmail.com>
2022-06-13 22:01:22 -0300

    Add lesson 20

---
+++ aula-20/funcoesString.js
@@ -0,0 +1,14 @@
+let nome = 'Xuxinha'
+// console.log(nome.length)
+
+const sobrenome = ' Nena '
+// console.log(sobrenome.length)
+let sobrenomeSemEspaco = sobrenome.trim()
+// console.log(sobrenomeSemEspaco.length)
+
+let cpf = '10667965480'
+console.log(cpf.slice(0, 3))
+
+let times = 'Corinthians,Flamengo,Palmares,Vaxco'
+console.log(times)
+console.log(times.split(','))
