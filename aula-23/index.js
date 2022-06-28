83469d7914df11815dc18dd84c81c19cf7628e64
Daniel Cavalcante de Souza <danielcdesouza@gmail.com>
2022-06-28 00:14:49 -0300

    Add new structure for lesson 23

--- aula-23/index.js
+++ aula-23/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const porta = 1500
+const routes = require('./routes/rotas')
 
 app.get('/', (req, res) => {
     res.send('Hello, Xuxinha')
 })
+
+app.use('/', routes)
 
 app.listen(porta, () => {
     console.log(`Serviço executando na porta: ${porta}`)
