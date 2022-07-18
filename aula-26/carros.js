802e8c75746948e3824299ba0b1ce3af42480369
Daniel Cavalcante <danielcdesouza@gmail.com>
2022-07-18 11:17:03 -0300

    Up lesson 28

---
+++ aula-26/carros.js
@@ -0,0 +1,39 @@
+const carros = [
+    {
+        id: 1,
+        marca: "Volksvagen Polo",
+        cor: "Preto",
+        ano: 2022,
+        doAno: true
+    },
+    {
+        id: 2,
+        marca: "Nissan March",
+        cor: "Vermelho",
+        ano: 2021,
+        doAno: false
+    },
+    {
+        id: 3,
+        marca: "Volksvagen Gol",
+        cor: "Prata",
+        ano: 2020,
+        doAno: false
+    },
+    {
+        id: 4,
+        marca: "Chevrolet Onix",
+        cor: "Preto",
+        ano: 2019,
+        doAno: false
+    },
+    {
+        id: 5,
+        marca: "Volksvagen Polo",
+        cor: "Branco",
+        ano: 2023,
+        doAno: true
+    }
+]
+
+module.exports = carros
--- aula-26/index.js
+++ aula-26/index.js
@@ -1,17 +1,49 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const carros = require('./carros.js')
+// JavaScript
+app.use(express.json());
 
-app.get('/filtro/:busca', (req, res) => {
-    const busca = req.params.busca
-    res.send('Sua busca foi: ' + busca)
+app.get('/filtro/:marca', (req, res) => {
+    const marca = req.params.marca
+    console.log(marca)
+    const resultado = carros.filter((carro) => {
+        return carro.marca === marca
+    })
+    if (resultado.length > 0) {
+        res.send(resultado)
+    }
+    res.send(`Não encontramos nenhum carro para a marca: ${marca}`)
 })
 
 app.get('/filtro', (req, res) => {
-    const busca = req.query.imovel
-    const query = req.query.q
-    const banda = req.query.banda
-    res.send(`Sua busca foi: ${busca}, ${query}, ${banda}`)
+    const marca = req.query.marca
+    const ano = req.query.ano
+    const cor = req.query.cor
+    const doAno = req.query.doAno
+
+    const resultado = carros.filter((carro) => {
+        return carro.marca === marca || carro.ano == ano || carro.cor === cor || carro.doAno === doAno
+    })
+
+    if (resultado.length > 0) {
+        res.send(resultado)
+    }
+    res.send('Não encontramos nenhum carro com as informações informadas.')
 })
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id
+    const novosCarros = carros.filter((carro) => carro.id != id)
+    res.send(novosCarros)
+})
+
+app.put('/update/:id', (req, res) => {
+    const carro = req.body
+    res.send(carro)
+})
+
+// CRUD = Create (Post), Read (Get), Update (Put) and Delete (Delete)
 
 app.listen(PORT, () => console.log('API está funcionando corretamente.'))
