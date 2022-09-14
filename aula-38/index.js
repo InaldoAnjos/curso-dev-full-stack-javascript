9cd4810e1f0e54185cf106e743d0971309bb9445
danielcdesouza@gmail.com <Daniel Cavalcante>
2022-09-13 22:46:28 -0300

    Finish up lesson 38

--- aula-38/index.js
+++ aula-38/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 const PORT = process.env.PORT;
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 app.get('/', (req, res) => {
     res.send('Hello, Xuxinha')
 })
 
-app.listen(PORT, () => {
+app.get('/usuarios', async (req, res) => {
+    const usuarios = await prisma.usuarios.findMany()
+    res.send(usuarios)
+})
+
+app.listen(3000, () => {
     console.log(`🚀 Serviço executando em: http://localhost:${PORT}`);
 })
 
