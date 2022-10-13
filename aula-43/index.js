d34b47fa6741bf1339894736e20ead8cd0b3e03e
Inaldo Anjos <inaldoanjosjr@gmail.com>
2022-10-13 19:54:43 -0300

    Update aulas 41 e 43

---
+++ aula-43/index.js
@@ -0,0 +1,6 @@
+const { default: mongoose } = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+
+//const Cat = mongoose.model('Cat', {name: String});
+
+//const kitty = new Cat
